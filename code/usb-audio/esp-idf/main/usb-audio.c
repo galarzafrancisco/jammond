0fe1ce201a99d2488826cbb7687253cc1de03baf
Francisco Galarza <franciscogalarza@gmail.com>
2025-02-09 14:09:29 +1100

    mac > tinyusb > i2s > dac

--- code/usb-audio/esp-idf/main/usb-audio.c
+++ code/usb-audio/esp-idf/main/usb-audio.c
@@ -22,7 +22,6 @@ PIN LAYOUT
 ----------------------------------------------------------------------------------------------------------------------------- */
 
 #define I2S_PORT I2S_NUM_0 // Use I2S0
-#define SAMPLE_RATE 44100  // Audio sample rate
 #define BITS_PER_SAMPLE 16 // PCM bit depth - THIS IS NOT A VARIABLE! DON'T CHANGE IT!
 #define PI 3.14159265
 #define SINE_WAVE_FREQ 220 // A4 note (440 Hz)
@@ -49,17 +48,23 @@ static void uac_device_set_volume_cb(uint32_t volume, void *arg)
   ESP_LOGI(TAG, "uac_device_set_volume_cb: %" PRIu32 "", volume);
 }
 
+volatile uint32_t samples_sent = 0;
 static esp_err_t uac_device_output_cb(uint8_t *buf, size_t len, void *arg)
 {
   size_t bytes_written;
 
   // buf contains interleaved stereo samples in 16-bit PCM format
-  int16_t *samples = (int16_t *)buf;          // Treat buffer as an array of 16-bit integers
-  size_t num_samples = len / sizeof(int16_t); // Convert bytes to number of samples
+  int16_t *samples = (int16_t *)buf; // Treat buffer as an array of 16-bit integers
 
   // Send samples to I2S
   i2s_channel_write(tx_handle, samples, len, &bytes_written, portMAX_DELAY);
 
+  if (bytes_written != len)
+  {
+    ESP_LOGI(TAG, "Received %d bytes from USB, wrote %d bytes to I2S. Drift!", len, bytes_written);
+  }
+
+  samples_sent += bytes_written / 2 / 2; // 2 bytes per channel (16 bits), 2 channels (stereo)
   // ESP_LOGI(TAG, "Received %d bytes from USB, wrote %d bytes to I2S. Samples: %d", len, bytes_written, num_samples);
 
   return ESP_OK;
@@ -77,16 +82,16 @@ void generate_sine_wave()
   int16_t sample[2];
   int amplitude = 32767 / 6; // Max value for 16-bit audio (limit to 25% so I don't break my ears)
 
-  for (int i = 0; i < SAMPLE_RATE; i++)
+  for (int i = 0; i < CONFIG_UAC_SAMPLE_RATE; i++)
   {
-    float theta = (2.0 * PI * SINE_WAVE_FREQ * i) / SAMPLE_RATE;
+    float theta = (2.0 * PI * SINE_WAVE_FREQ * i) / CONFIG_UAC_SAMPLE_RATE;
     int16_t value = (int16_t)(amplitude * sin(theta));
 
     // Stereo sample (L, R)
     sample[0] = value;
     sample[1] = value;
 
-    i2s_channel_write(tx_handle, sample, sizeof(sample), &bytes_written, portMAX_DELAY);
+    ESP_ERROR_CHECK(i2s_channel_write(tx_handle, sample, sizeof(sample), &bytes_written, portMAX_DELAY));
   }
 }
 
@@ -102,12 +107,14 @@ void init_i2s_dac_driver()
   // Step 1: Define the I2S channel configuration
   i2s_chan_config_t chan_cfg = I2S_CHANNEL_DEFAULT_CONFIG(I2S_NUM_0, I2S_ROLE_MASTER);
 
-  chan_cfg.dma_desc_num = 10;
+  // buffer_size = dma_frame_num * slot_num * slot_bit_width / 8
+  chan_cfg.dma_desc_num = 6;    // default 6
+  chan_cfg.dma_frame_num = 128; // default 240
   ESP_ERROR_CHECK(i2s_new_channel(&chan_cfg, &tx_handle, NULL));
 
   // Step 2: Define the standard mode configuration
   i2s_std_config_t std_cfg = {
-      .clk_cfg = I2S_STD_CLK_DEFAULT_CONFIG(SAMPLE_RATE),
+      .clk_cfg = I2S_STD_CLK_DEFAULT_CONFIG(CONFIG_UAC_SAMPLE_RATE),
       .slot_cfg = I2S_STD_PHILIPS_SLOT_DEFAULT_CONFIG(I2S_DATA_BIT_WIDTH_16BIT, I2S_SLOT_MODE_STEREO),
       .gpio_cfg = {
           .mclk = I2S_GPIO_UNUSED, // Use I2S_GPIO_UNUSED if not used
@@ -143,11 +150,28 @@ void init_usb_uac_audio_device()
 Main
 
 ----------------------------------------------------------------------------------------------------------------------------- */
+void monitor_audio_sync(void *arg)
+{
+  while (1)
+  {
+    static uint32_t last_samples_sent = 0;
+    uint32_t expected_samples = 48000; // We should have played this much in 1 sec
+    uint32_t actual_samples_sent = samples_sent - last_samples_sent;
+
+    ESP_LOGI(TAG, "Expected: %d, Actual Sent: %d, Diff: %d",
+             expected_samples, actual_samples_sent, actual_samples_sent - expected_samples);
+
+    last_samples_sent = samples_sent;
+    vTaskDelay(pdMS_TO_TICKS(1000)); // Run every 1 second
+  }
+}
 
 void app_main(void)
 {
   init_i2s_dac_driver();
   init_usb_uac_audio_device();
+  xTaskCreate(monitor_audio_sync, "monitor_audio_sync", 4 * 1024, NULL, 1, NULL);
+
   // while (1)
   // {
   //   generate_sine_wave();
